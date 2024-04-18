a5bc7a16950f5cc8a036c417e6ba2937be508607
paulocsa <paulocandiani04@gmail.com>
2024-04-17 21:02:00 -0300

    .

--- NodeJS/node.js-05.1-conexao-com-banco-de-dados-mysql-com-sequelize/controllers/ClientesController.js
+++ NodeJS/node.js-05.1-conexao-com-banco-de-dados-mysql-com-sequelize/controllers/ClientesController.js
@@ -1,20 +1,15 @@
 import express from 'express';
 const router = express.Router();
+import Cliente from '../models/Cliente.js';
 
 //ROTA CLIENTES
 
-router.get("/clientes" , (req, res) => {
-
-    const clientes = [
-        {nome: "Ana Silva", cpf: "123.456.789-00", endereco: "Rua das Flores, 123, Bairro Jardim Primavera, Cidade Felicidade, Estado do Sonho, CEP: 12345-678"},
-        {nome: "Pedro Almeida", cpf: "987.654.321-00", endereco: "Avenida Central, 456, Bairro Centro, Cidade Nova, Estado da Esperança, CEP: 98765-432"},
-        {nome: "Marina Oliveira", cpf: "456.789.123-00", endereco: "Travessa dos Sonhos, 789, Bairro Vista Linda, Cidade Sol Nascente, Estado da Harmonia, CEP: 54321-987"},
-        {nome: "Rafael Santos", cpf: "321.654.987-00", endereco: "Praça da Amizade, 321, Bairro Bela Vista, Cidade Alegria, Estado da Serenidade, CEP: 87654-321"}
-    ]
-    res.render("clientes", {
-        clientes : clientes
-    });
-
+router.get("/clientes", (req, res) => {
+    Cliente.findAll().then(clientes => {
+        res.render("clientes", {
+            clientes:clientes
+        });
+    })
 });
 
 
